f40e89db0d36aa42ed30bd45ea9ae9c549cfd1b1
zerrinayz <zerrinayaz76@gmail.com>
2025-01-03 22:11:58 +0300

    categorical analysis and correlation visualizations

--- data_science.py
+++ data_science.py
@@ -286,3 +286,96 @@ def plotBox(degisken):
 sayisal_degiskenler = ["yas", "boy", "kilo"]
 for degisken in sayisal_degiskenler:
     plotBox(degisken)
+# %% kategorik degiskenler
+veri = pd.read_csv("olimpiyatlar_temizlenmis.csv")
+def plotBar(degisken, n = 5): # en cok 5 adet veriyi gorsellestir
+
+    veri_ = veri[degisken]
+    veri_sayma = veri_.value_counts() # value counts
+    veri_sayma = veri_sayma[:n]
+    
+    plt.figure()
+    plt.bar(veri_sayma.index, veri_sayma, color = "orange")
+    plt.xticks(veri_sayma.index, veri_sayma.index.values)
+    plt.xticks(rotation = 45)
+    plt.ylabel("Frekans")
+    plt.title(f"Veri Frekansi: {degisken}")
+    plt.show()
+    print(f"{veri_sayma}")
+
+# dataframe icierisnde istenilen tipteki degiskenleri bul
+categorical_columns = veri.select_dtypes(include = ["object"]).columns
+
+for degisken in categorical_columns:
+    plotBar(degisken, 20)
+    
+# %% iki degiskenli veri analizi
+
+# cinsiyete gore boy ve kilo karsilastirmasi
+
+erkek = veri[veri.cinsiyet == "M"]
+kadin = veri[veri.cinsiyet == "F"]
+
+plt.figure()
+plt.scatter(kadin.boy, kadin.kilo, alpha = 0.8, label = "Kadin")
+plt.scatter(erkek.boy, erkek.kilo, alpha = 0.1, label = "Erkek")
+plt.xlabel("boy")
+plt.ylabel("kilo")
+plt.title("Boy ve Kilo Arasındaki İlişki")
+plt.legend()
+
+# correlation calculation
+numeric_correlation = veri.loc[:, ["yas", "boy", "kilo"]].corr() 
+
+# madalya ve yas arasindaki correlation
+veri_gecici = veri.copy()
+veri_gecici = pd.get_dummies(veri_gecici, columns = ["madalya"])
+numeric_correlation_yas_madalya = veri_gecici.loc[:, ["yas", 'madalya_Bronze', 'madalya_Gold','madalya_Silver']].corr()
+
+# takimlarin kazandiklari altin gumus ve vronz madalya sayilari 
+# groupby
+# frupladik, toplamini aldik, siraladik ve ilk 20 sini elde ettik
+veri_gecici["takim"] = veri_gecici["takim"].replace({ # soviet union isimlerini rusya ile replace et degistir.
+    "Soviet Union": "Russia" 
+})
+groupby_takim = veri_gecici[["takim", "madalya_Gold", "madalya_Silver", "madalya_Bronze"]].groupby(["takim"], as_index = False).sum()
+groupby_takim_sorted = groupby_takim.sort_values(by = "madalya_Gold", ascending = False)
+groupby_takim_sorted_10 = groupby_takim_sorted[:20]
+
+turkey = groupby_takim.query("takim == 'Turkey'")  # Sadece Türkiye'yi filtrele
+
+# sehirlere gore kazanilan madalyalarin ortalamalari
+groupby_sehir = veri_gecici[["sehir","madalya_Bronze","madalya_Silver","madalya_Gold"]].groupby(["sehir"],as_index=False).sum().sort_values(by = "madalya_Gold", ascending = False)
+
+# cinsiyete gore
+groupby_cinsiyet = veri_gecici[["cinsiyet","madalya_Bronze","madalya_Silver","madalya_Gold"]].groupby(["cinsiyet"],as_index=False).sum().sort_values(by = "madalya_Gold", ascending = False)
+
+# %% cok degiskenli veri analizi
+# pivot table
+
+# madalya alan sporcularin cinsiyetlerine gore boy, kilo ve yas ortalamalarina bakalim
+# 3 adet madalya, 2(cinsiyet)*3(boy, kilo yas)*3(mean, max, min) = 18
+veri_pivot = veri.pivot_table(index = "madalya",
+                              columns = "cinsiyet",
+                              values = ["boy", "kilo", "yas"],
+                              aggfunc = {"boy": np.mean, 
+                                         "kilo": [np.median, np.max],
+                                         "yas": [np.min, np.max, np.std]})
+
+# takimlara ve cinsiyete gore alinan madalya sayilarinin toplami, max ve min degerleri
+
+#takımlara ve cinsiyete gore alınan madalya sayıların toplamı ve maks ve min değeleri
+veri_pivot_takim = veri_gecici.pivot_table(index=["takim", "sehir"], 
+                                        columns = ["cinsiyet", "sezon"],
+                 values=["madalya_Gold","madalya_Silver","madalya_Bronze"], 
+                aggfunc={"madalya_Gold":[np.sum],
+                         "madalya_Silver":[np.sum],
+                         "madalya_Bronze":[np.sum]})
+
+veri_pivot_takim["total"] = (
+    veri_pivot_takim["madalya_Gold"].sum(axis =1) +
+    veri_pivot_takim["madalya_Silver"].sum(axis =1) + 
+    veri_pivot_takim["madalya_Bronze"].sum(axis =1))
+veri_pivot_takim = veri_pivot_takim.sort_values(by = "total", ascending = False)[:100]
+
+veri_pivot_takim.to_excel("veri_pivot_takim.xlsx")
